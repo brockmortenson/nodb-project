0e337e37eb0555f250d2e417a15e606d40b92d11
Brock Mortenson <mortensonbrock@gmail.com>
2021-03-12 20:33:58 -0700

    I've been trying to find on google exactly how the endpoint works when you are using an api created by you but I cannot find anything....

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 // Add port # to proxy in package.json
-
+const data = require('../src/data.json');
 const express = require('express');
 
 const app = express();
 
 app.use(express.json());
+
+
+app.get(`/api/data`)
+
 
 app.listen(3030, () => console.log('Server is running on 3030'))
